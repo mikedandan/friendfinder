11d0f93355a2de9ead38e840cfc0c1d23ac0a669
mikedandan <mikedandan2@gmail.com>
2019-02-17 22:11:20 -0800

    updated code for routes, html, and js files

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set the public folder path
+app.use(express.static("app/public"));
+
+//routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
