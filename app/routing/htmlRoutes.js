a416b9442c9c084738b99f8dd694614c81fa44b9
mikedandan <mikedandan2@gmail.com>
2019-02-21 15:50:38 -0800

    updated code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -20,6 +20,6 @@ module.exports = function (app) {
       
     // USE route returns home.html for any undefined GET routes.
     app.use(function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.sendFile(path.join(__dirname + "../public/home.html"));
     });
 };
